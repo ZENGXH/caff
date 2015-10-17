d6706517497116eba2c0b13f44b4f96d253173f8
Xiaohui Zeng <xizeng@icivrgsrv1.epfl.ch>
2015-10-18 00:41:07 +0200

    add read_net, make life easier

---
+++ python/caffe/read_net.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""help function:
+usage: import caffe.read_net
+layers = caffe.read_net('bvlc_reference_caffenet')
+
+read the deploy.prototxt file and generate the a list of layers
+by layers[i].name/type, we can easily get the imformation of the net
+"""
+from google.protobuf import text_format
+
+import caffe
+from caffe.proto import caffe_pb2
+
+PATH2MODEL = '../models/'
+prototxt = '/deploy.prototxt'
+
+def get(input_net_proto_file):
+
+    caffe_net = caffe_pb2.NetParameter()
+    input_net_proto_file = PATH2MODEL + input_net_proto_file + prototxt
+
+    text_format.Merge(open(input_net_proto_file).read(), caffe_net)
+
+    print('reading net from %s' % input_net_proto_file)
+
+    layers = [];
+
+    for layer in caffe_net.layer:
+	layers.append(layer)	
+    return layers
+
