87692498b4b02f23e24645be0b9f2be09fe403fb
Xiaohui Zeng <xzengaf@ust.hk>
2015-11-16 17:10:24 +0100

    use ave pooling

--- examples/cifar10/train_my.sh
+++ examples/cifar10/train_my.sh
@@ -3,9 +3,9 @@
 TOOLS=./build/tools
 
 $TOOLS/caffe train \
-  --solver=examples/cifar10/myNet_solver.prototxt
+  --solver=examples/cifar10/myNet_solver1116.prototxt
+#  --snapshot=examples/cifar10/snapshot/myNet3_iter_2100.solverstate.h5
 
 # reduce learning rate by factor of 10 after 8 epochs
 #$TOOLS/caffe train \
 #  --solver=examples/cifar10/cifar10_quick_solver_lr1.prototxt \
-#  --snapshot=examples/cifar10/cifar10_quick_iter_4000.solverstate.h5
